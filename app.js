49e6ec9b087e7714e1d756fbba14e99f00812e8c
belachkar <belachkar_ali@hotmail.com>
2019-10-17 09:02:35 +0100

    Add edit function Add toast messages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express'),
   env = require('dotenv').config(),
   path = require('path'),
   cons = require('consolidate'),
+  // eslint-disable-next-line no-unused-vars
   dust = require('dustjs-helpers');
   
 const app = express();
@@ -34,7 +35,9 @@ app.get('/', (req, res) => {
   
   client.connect()
     .then(() => {
-      client.query('SELECT * FROM recipes', (err, result) => {
+      const query = 'SELECT * FROM recipes';
+
+      client.query(query, (err, result) => {
         if(err) {
           errors.push({ error: 'Error: a connection problem, can not get recipes!' });
           console.error(err.message);
@@ -73,7 +76,9 @@ app.post('/add', (req, res) => {
     
   client.connect()
     .then(() => {
-      client.query('INSERT INTO recipes (name, ingredients, directions) VALUES ($1, $2, $3)', values, (err, result) => {
+      const query = 'INSERT INTO recipes (title, ingredients, directions) VALUES ($1, $2, $3)';
+
+      client.query(query, values, (err, result) => {
         if(err) {
           errors.push({ error: 'Error: a connection problem, could not add the new recipe!' });
           tmpFields.push({ title, ingredients, directions });
@@ -81,8 +86,11 @@ app.post('/add', (req, res) => {
           client.end();
           return res.redirect('/');
         }
-        if(result.rowCount < 1) errors.push('Adding new recipe operation failed!');
-        console.log(result.rowCount);
+        if(result.rowCount < 1) {
+          errors.push('Adding new recipe operation failed!');
+        } else {
+          console.log(result.rowCount);
+        }
         message = 'Recipe add';
         client.end();
         return res.redirect('/');
@@ -92,7 +100,7 @@ app.post('/add', (req, res) => {
 });
 
 app.delete('/delete/:id', (req, res) => {
-  let id = req.params.id;
+  const id = req.params.id;
   
   if(!id) {
     console.log('DELETE: The ID is missing!');
@@ -108,16 +116,70 @@ app.delete('/delete/:id', (req, res) => {
     
   client.connect()
     .then(() => {
-      client.query('DELETE FROM recipes WHERE id = $1', values, (err, result) => {
+      const query = 'DELETE FROM recipes WHERE id = $1';
+
+      client.query(query, values, (err, result) => {
         if(err) {
           errors.push({ error: 'Error: a connection problem, the recipe was not deleted!' });
           console.error(err.message);
           client.end();
           return res.redirect('/');
         }
-        if(result.rowCount < 1) errors.push('Delete operation failed!');
+        if(result.rowCount < 1) {
+          errors.push('Delete operation failed!');
+        } else {
+          message = 'Recipe removed';
+        }
         console.log(result.rowCount);
-        message = 'Recipe removed';
+        client.end();
+        return res.redirect('/');
+      });
+    }, onConnectionRejected)
+    .catch(err => { throw err.message; });  
+});
+
+app.post('/edit', (req, res) => {
+  let { id, title, ingredients, directions } = req.body;  
+  id = id.trim();
+  title = title.trim();
+  ingredients = ingredients.trim();
+  directions = directions.trim();
+  
+  if(!id) {
+    console.log('UPDATE: The ID is missing!');
+    errors.push({ error: 'The ID of the recipe is missing!' });
+    return res.redirect('/');
+  }
+  
+  if(!title || !ingredients || !directions) {
+    console.log('UPDATE: A field value is missing!');
+    errors.push({ error: 'All the fields are required!' });
+    return res.redirect('/');
+  }
+
+  const values = [title, ingredients, directions, id];
+
+  // Start connection to DB
+  let client = new Client();
+  client.on('error', (err) => console.error(err.message));
+    
+  client.connect()
+    .then(() => {
+      const query = 'UPDATE recipes SET title=$1, ingredients=$2, directions=$3 WHERE id = $4';
+
+      client.query(query, values, (err, result) => {
+        if(err) {
+          errors.push({ error: 'Error: a connection problem, the recipe was not updated!' });
+          console.error(err.message);
+          client.end();
+          return res.redirect('/');
+        }
+        if(result.rowCount < 1) {
+          errors.push('Update operation failed!');
+        } else {
+          message = 'Recipe updated';
+        }
+        console.log(result.rowCount);
         client.end();
         return res.redirect('/');
       });
@@ -129,5 +191,5 @@ function onConnectionRejected(err) {
   console.error(err.message);
 }
 
-// Server
+// Launch Server
 app.listen(srvPort, () => console.log('Server started On port '+srvPort));
--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,65 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
 
   const actionBtns = [
-    { deleteRecipe: {
-      selector: '.delete-recipe',
-      action: ''
-    } }
+    { name: 'Delete Recipe',
+      method: 'DELETE',
+      url: '/delete/',
+      btnSelector: '.delete-recipe',
+      confirmMsg: 'Delete Recipe?',
+      op: 'delete',
+    }, {
+      name: 'Update Recipe',
+      method: 'PUT',
+      url: '/put/',
+      btnSelector: '.update-recipe',
+      confirmMsg: null,
+      op: 'update'
+    }, {
+      name: 'Edit Recipe',
+      method: null,
+      url: null,
+      btnSelector: '.edit-recipe',
+      confirmMsg: null,
+      op: 'edit'
+    }
   ];
-  const deleteRecepeBtns = document.querySelectorAll('.delete-recipe');
-  const editRecipeBtn = document.querySelector('.edit-recipe');
 
-  deleteRecepeBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      const id = e.currentTarget.getAttribute('data-id');
-      const url = '/delete/'+id;
+  actionBtns.forEach(action => {
+    const btns = document.querySelectorAll(action.btnSelector);
 
-      if (confirm('Delete Recipe?')) {
-        sendDelete(url);
-      }
+    if(!btns) return;
+    btns.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const id = e.currentTarget.getAttribute('data-id');
+        const op = action.op;
+        const method = action.method;
+        const url = action.url ? action.url+id : null;
+        const confirmMsg = action.confirmMsg;
+
+        console.log(id);
+        console.log(op);
+
+        switch (op) {
+        case 'delete':
+          if(confirmMsg) {
+            if (confirm(confirmMsg)) sendRequest(url, method);
+          }
+          break;
+        case 'update':
+          sendRequest(url, method);
+          break;
+        case 'edit':
+          document.getElementById('editId').value = e.currentTarget.getAttribute('data-id');
+          document.getElementById('editTitle').value = e.currentTarget.getAttribute('data-title');
+          document.getElementById('editIngredients').value = e.currentTarget.getAttribute('data-ingredients');
+          document.getElementById('editDirections').value = e.currentTarget.getAttribute('data-directions');
+        }
+      });
     });
   });
 
-  async function sendDelete(url) {
-    await fetch(url, { method: 'DELETE' });
+  async function sendRequest(url, method) {
+    await fetch(url, { method });
+    location.href = '/';
   }
 });
